a21d8e176904a7dc39cd5213f5a59e8d33a4d68e
aliyang <aliyang@aliyang.com>
2018-06-19 21:15:40 +0800

    update

--- src/T147_1.java
+++ src/T147_1.java
@@ -8,28 +8,41 @@ public class T147_1 {
 
     public double findMedianSortedArrays(int A[], int B[]) {
 
-        int n=A.length;
-        int m=B.length;
-        if (n>m)//保证数组1一定是短的
+        int m=A.length;
+        int n=B.length;
+
+        if (m<n)//保证A是长的
             return findMedianSortedArrays(B,A);
-        int L1=0,L2=0,R1=0,R2=0,c1,c2;//L表示割点左边部分的最大值，R表示割点右边部分的最大值,C为割点
-        int lo=0;
-        int hi=2*n;//假设虚拟加了'#'，数组1是2*n的长度
+        if (n==0)
+            return (double)(A[(m-1)/2]+A[(m/2)])/2.0;
 
-        while (lo<hi){//开始二分
-            c1=(lo+hi)/2;//c1二分，
-            c2=m+n-c1;//因为找第m+n小的，所以减去C1的割点
-            L1=(c1==0)?Integer.MIN_VALUE:A[(c1-1)/2];//如果A数组到了最左边，说明A数组的数都比B的大，说明中位数再B数组中
-            R1=(c1==2*n)?Integer.MAX_VALUE:A[c1/2];
-            L2 = (c2 == 0)?Integer.MIN_VALUE:B[(c2-1)/2];
-            R2 = (c2 == 2*m)?Integer.MAX_VALUE:B[c2/2];
-
-            if (L1>R2)//左边缩
-                hi=c1-1;
-            else if (L2>R1)
-                lo=c1+1;
-            else break;
+        int lo = 0, hi = 2 * n, c2 = n;
+        while(c2 >= 0 && c2 <= 2*n) {
+            int c1 = m + n - c2;
+            double L1 = (c1 == 0) ? Integer.MIN_VALUE : A[(c1 - 1)/2];
+            double L2 = (c2 == 0) ? Integer.MIN_VALUE : B[(c2 - 1)/2];
+            double R1 = (c1 == 2 * m) ? Integer.MAX_VALUE : A[c1/2];
+            double R2 = (c2 == 2 * n) ? Integer.MAX_VALUE : B[c2/2];
+            if(L1 > R2) c2++;
+            else if(L2 > R1) c2--;
+            else return (double)(Math.max(L1,L2) + Math.min(R1,R2)) / 2.0;
         }
-        return (Math.max(L1,L2)+Math.min(R1,R2))/2.0;
+        return -1;
+    }
+
+    private double singleArrayFindMedian(int[] A){
+
+        int n=A.length;
+        if (n%2==1)
+            return 1.0*A[n/2];
+        else return (A[n/2-1]+A[n/2])/2.0;
+    }
+
+    public static void main(String[] args){
+        T147_1 t=new T147_1();
+        int[] A=new int[]{3};
+        int[] B=new int[]{1,2};
+        System.out.println(t.findMedianSortedArrays(A,B));
+
     }
 }
